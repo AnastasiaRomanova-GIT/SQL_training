27ca563c421940e11a3f1b7ab40fbc81b2fb3f97
VousataAgama <aorrao@gmail.com>
2024-03-11 17:23:45 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -84,3 +84,57 @@ WHERE
 
 SELECT * 
 FROM fine
+
+/*Создать таблицу author следующей структуры:
+
+Поле	Тип, описание
+author_id	INT PRIMARY KEY AUTO_INCREMENT
+name_author	VARCHAR(50)*/
+CREATE TABLE author (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_author VARCHAR(50)
+                    )
+;
+
+/*Заполнить таблицу author. В нее включить следующих авторов:
+
+Булгаков М.А.
+Достоевский Ф.М.
+Есенин С.А.
+Пастернак Б.Л.*/
+INSERT INTO author(author_id, name_author)
+VALUES (1, 'Булгаков М.А.'),
+        (2, 'Достоевский Ф.М.'),
+        (3, 'Есенин С.А.'),
+        (4, 'Пастернак Б.Л.');
+        
+SELECT * FROM author
+;
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT, 
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id)  REFERENCES author (author_id),
+    FOREIGN KEY (genre_id)  REFERENCES genre (genre_id) 
+                    );
+                    
+SHOW COLUMNS FROM book;
+
+/*Задание
+Создать таблицу book той же структуры, что и на предыдущем шаге. Будем считать, что при удалении автора из таблицы author, должны удаляться все записи о книгах из таблицы book, написанные этим автором. А при удалении жанра из таблицы genre для соответствующей записи book установить значение Null в столбце genre_id. */
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT, 
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id)  REFERENCES author (author_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id)  REFERENCES genre (genre_id) ON DELETE SET NULL
+                    );
+                    
+SHOW COLUMNS FROM book;  
