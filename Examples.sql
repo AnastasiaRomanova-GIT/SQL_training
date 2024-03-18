e7027d81ba11b26b3c365297ee22ae39c061bbcd
VousataAgama <aorrao@gmail.com>
2024-03-18 20:10:10 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -179,3 +179,38 @@ FROM book INNER JOIN author
 GROUP BY name_author
 HAVING COUNT(DISTINCT genre_id) = 1
 ORDER BY name_author
+
+/*Вывести информацию о книгах (название книги, фамилию и инициалы автора, название жанра, цену и количество экземпляров книг), написанных в самых популярных жанрах, в отсортированном в алфавитном порядке по названию книг виде. Самым популярным считать жанр, общее количество экземпляров книг которого на складе максимально.*/
+
+SELECT title, name_author, name_genre, price, amount
+FROM 
+    author 
+    INNER JOIN book ON author.author_id = book.author_id
+    INNER JOIN genre ON  book.genre_id = genre.genre_id
+
+WHERE genre.genre_id IN
+         (SELECT query_in_1.genre_id
+          FROM 
+              (SELECT genre_id, SUM(amount) AS sum_amount
+               FROM book
+               GROUP BY genre_id) query_in_1
+          INNER JOIN 
+              (SELECT genre_id, SUM(amount) AS sum_amount
+               FROM book
+               GROUP BY genre_id
+               ORDER BY sum_amount DESC
+               LIMIT 1) query_in_2
+          ON query_in_1.sum_amount = query_in_2.sum_amount)
+
+ORDER BY title
+;
+
+/*Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену,  вывести их название и автора, а также посчитать общее количество экземпляров книг в таблицах supply и book,  столбцы назвать Название, Автор  и Количество.*/
+SELECT book.title AS Название, name_author AS Автор, (book.amount + supply.amount) AS Количество
+FROM 
+    author
+    INNER JOIN book USING(author_id)
+    INNER JOIN supply ON author.name_author = supply.author
+                      AND book.title = supply.title
+
+WHERE book.amount = supply.amount
