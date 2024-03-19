5e26f84b2ac1ee008680d94a711abadeea6e5f05
Anastasia Romanova <83038100+AnastasiaRomanova-GIT@users.noreply.github.com>
2024-03-19 10:27:31 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -230,3 +230,18 @@ FROM
                       AND book.title = supply.title
 
 WHERE book.amount = supply.amount
+
+
+/*Для книг, которые уже есть на складе (в таблице book), но по другой цене, чем в поставке (supply),  необходимо в таблице book увеличить количество на значение, указанное в поставке,  и пересчитать цену. А в таблице  supply обнулить количество этих книг. Формула для пересчета цены: price_new = (price1*amount1 + price2*amount2)/(amount1 + price 1)*/
+UPDATE book 
+     INNER JOIN author ON author.author_id = book.author_id
+     INNER JOIN supply ON book.title = supply.title 
+                         and supply.author = author.name_author
+SET book.price = ((book.amount*book.price + supply.amount*supply.price)/(book.amount + supply.amount)),
+    book.amount = book.amount + supply.amount,
+    supply.amount = 0   
+WHERE book.price <> supply.price;
+
+SELECT * FROM book;
+
+SELECT * FROM supply;
