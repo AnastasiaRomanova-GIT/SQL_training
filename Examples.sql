f605243d809b211b3fc1ad99c9a81656a57b82b9
Anastasia Romanova <83038100+AnastasiaRomanova-GIT@users.noreply.github.com>
2024-03-21 09:36:24 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -245,3 +245,23 @@ WHERE book.price <> supply.price;
 SELECT * FROM book;
 
 SELECT * FROM supply;
+
+/*Включить новых авторов в таблицу author с помощью запроса на добавление, а затем вывести все данные из таблицы author.  Новыми считаются авторы, которые есть в таблице supply, но нет в таблице author.*/
+INSERT INTO author(name_author)
+SELECT supply.author
+FROM 
+    author 
+    RIGHT JOIN supply on author.name_author = supply.author
+WHERE name_author IS Null;
+
+SELECT * FROM author
+
+/*Добавить новые книги из таблицы supply в таблицу book на основе сформированного выше запроса. Затем вывести для просмотра таблицу book.*/
+INSERT INTO book (title, author_id, price, amount)
+SELECT title, author_id, price, amount
+FROM 
+    author 
+    INNER JOIN supply ON author.name_author = supply.author
+WHERE amount <> 0;
+
+SELECT * FROM book
