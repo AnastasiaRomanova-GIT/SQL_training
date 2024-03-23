a5ec2520bc732a12a5f53f0251af651700e67299
Anastasia Romanova <83038100+AnastasiaRomanova-GIT@users.noreply.github.com>
2024-03-23 09:54:26 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -311,3 +311,17 @@ WHERE author_id IN
 SELECT * FROM author;
 
 SELECT * FROM book;
+
+/*Удалить все жанры, к которым относится меньше 4-х наименований книг. В таблице book для этих жанров установить значение Null.*/
+DELETE
+FROM genre 
+WHERE genre_id IN 
+                    (SELECT genre_id
+                    FROM book 
+                    GROUP BY genre_id
+                    HAVING COUNT(DISTINCT book_id) < 4
+                    );
+
+SELECT * FROM genre;
+
+SELECT * FROM book;
