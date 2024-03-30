11feee7eaf5797f57f4115b1da44ce74fe477c4e
VousataAgama <aorrao@gmail.com>
2024-03-30 07:41:36 +0100

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -336,3 +336,17 @@ FROM
     INNER JOIN book ON buy_book.book_id=book.book_id
 WHERE name_client ='Баранов Павел'
 ORDER BY buy.buy_id, book.title; 
+
+/*Вывести города, в которых живут клиенты, оформлявшие заказы в интернет-магазине. Указать количество заказов в каждый город, этот столбец назвать Количество. Информацию вывести по убыванию количества заказов, а затем в алфавитном порядке по названию городов.*/
+SELECT city.name_city, COUNT(DISTINCT buy.buy_id) AS Количество
+FROM city
+    INNER JOIN client ON city.city_id = client.city_id
+    INNER JOIN buy ON client.client_id = buy.client_id
+GROUP BY city.city_id 
+ORDER BY Количество DESC, city.name_city; 
+
+/*Вывести номера всех оплаченных заказов и даты, когда они были оплачены.*/
+SELECT buy_id, date_step_end
+FROM buy_step
+    INNER JOIN step ON buy_step.step_id = step.step_id
+WHERE buy_step.step_id = 1 AND buy_step.date_step_end IS NOT NULL 
