8deb871622587fb483419110f9ff90e3c4f814b9
Anastasia Romanova <83038100+AnastasiaRomanova-GIT@users.noreply.github.com>
2024-03-31 09:51:43 +0200

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -362,3 +362,11 @@ GROUP BY author.name_author, book.title, buy_book.book_id
 ORDER BY author.name_author, book.title
 
 
+/*Вывести информацию о каждом заказе: его номер, кто его сформировал (фамилия пользователя) и его стоимость (сумма произведений количества заказанных книг и их цены), в отсортированном по номеру заказа виде. Последний столбец назвать Стоимость.*/
+SELECT buy_book.buy_id, name_client, SUM(buy_book.amount * book.price) AS Стоимость
+FROM book
+    INNER JOIN buy_book ON book.book_id = buy_book.book_id
+    INNER JOIN buy ON buy.buy_id = buy_book.buy_id
+    INNER JOIN client ON buy.client_id = client.client_id
+GROUP BY buy_book.buy_id
+ORDER BY buy_id 
