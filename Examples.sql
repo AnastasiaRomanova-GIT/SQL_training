aa17d9f36ed00f8458304539ee2e486fb1eb9503
Anastasia Romanova <83038100+AnastasiaRomanova-GIT@users.noreply.github.com>
2024-04-05 13:20:13 +0200

    Update Examples.sql

--- Examples.sql
+++ Examples.sql
@@ -380,3 +380,19 @@ FROM buy_step
     INNER JOIN city ON client.city_id = city.city_id
 WHERE step.name_step = "Транспортировка" AND buy_step.date_step_beg IS NOT NULL AND buy_step.date_step_end IS NOT NULL
 ORDER BY buy_step.buy_id
+
+/*Сравнить ежемесячную выручку от продажи книг за текущий и предыдущий годы. Для этого вывести год, месяц, сумму выручки в отсортированном сначала по возрастанию месяцев, затем по возрастанию лет виде. Название столбцов: Год, Месяц, Сумма.*/
+SELECT YEAR(buy_step.date_step_end) AS Год, MONTHNAME(buy_step.date_step_end) AS Месяц, SUM(buy_book.amount * book.price) AS Сумма
+FROM buy_step
+    INNER JOIN buy_book ON buy_step.buy_id = buy_book.buy_id
+    INNER JOIN book ON buy_book.book_id = book.book_id
+WHERE buy_step.step_id = 1 AND buy_step.date_step_end IS NOT NULL
+GROUP BY Год, Месяц
+
+UNION 
+
+SELECT YEAR(date_payment) AS Год, MONTHNAME(date_payment) AS Месяц, SUM(amount * price) AS Сумма
+FROM buy_archive
+GROUP BY Год, Месяц
+
+ORDER BY Месяц, Год
